8a08d5663463359b8c6c824020d73ec5f4f41a6b
Nehul Soni <soninehul@gmail.com>
2021-03-23 17:32:16 +0530

    Styling and Bug Fixes

--- src/StockCard.js
+++ src/StockCard.js
@@ -16,26 +16,32 @@ function StockCard({ id, stockName, removeStockData}) {
         async function getStockData(stockName){
             console.log(stockName);
             const response = await axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${stockName}&interval=15min&apikey=LOTL819YCVCGOBPV`);
+            const keys = Object.keys(response.data);
+            console.log(keys[0]);
+            if(response.data){
             const dayData = response.data["Time Series (15min)"];
             const dateTime = response.data["Meta Data"]["3. Last Refreshed"];
             console.log(dateTime);
             const lastData = dayData[dateTime];
             const stockPrice = lastData['4. close'];
             console.log(stockPrice);
-            setFinalData({name: stockName, price: stockPrice, dateAndTime: dateTime});
+            setFinalData({name: stockName, price: stockPrice, dateAndTime: dateTime});}
         }
         getStockData(stockName)
     },[stockName])
     return(
-        <div className= "stock-card">
+        <div>
+        {finalData.price && 
+            <div className= "stock-card">
             <ul>
                 <li><h4>{finalData.name}</h4></li>
-                <li><p>Price: {finalData.price}</p></li>
-                <li><p>Last Updated: {finalData.dateAndTime}</p></li>
+                <li>{finalData.price && <p>Price: {finalData.price}</p>}</li>
+                <li>{finalData.dateAndTime && <p>Last Updated: {finalData.dateAndTime}</p>}</li>
             </ul>
             <IconButton className="delete-icon" aria-label='Delete' onClick={() => removeStockData(id)}>
                     <DeleteIcon />
             </IconButton>
+        </div>}
         </div>
     )
 }
